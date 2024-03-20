38fc749f3062d57c2bd3705055f18897dd866a75
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-03-20 19:27:33 +0000

    update

--- app.rb
+++ app.rb
@@ -9,18 +9,20 @@ get("/") do
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
 
   # use HTTP.get to retrieve the API information
-  raw_data = HTTP.get(api_url)
+  #raw_data = HTTP.get(api_url)
 
   # convert the raw request to a string
-  raw_data_string = raw_data.to_s
+  #raw_data_string = raw_data.to_s
 
   # convert the string to JSON
-  parsed_data = JSON.parse(raw_data_string)
+  #parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
   # @symbols = parsed_data ...
 
   # render a view template where I show the symbols
   # erb(:homepage)
+
+  erb(:original_currency)
 
 end
