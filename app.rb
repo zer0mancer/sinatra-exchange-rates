d10c96246fa59df65a7b76e3d030b34da2f0d529
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-03-22 05:04:44 +0000

    broken

--- app.rb
+++ app.rb
@@ -1,28 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 require 'http'
-
-
+require 'json'
 
 get("/") do
+
   # build the API url, including the API key in the query string
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
 
   # use HTTP.get to retrieve the API information
-  #raw_data = HTTP.get(api_url)
+  raw_data = HTTP.get(api_url)
 
   # convert the raw request to a string
-  #raw_data_string = raw_data.to_s
+  raw_data_string = raw_data.to_s
 
   # convert the string to JSON
-  #parsed_data = JSON.parse(raw_data_string)
+  parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  # @symbols = parsed_data ...
+  @symbols = parsed_data.fetch("currencies").keys
 
   # render a view template where I show the symbols
-  # erb(:homepage)
-
-  erb(:original_currency)
-
+   erb(:homepage)
 end
