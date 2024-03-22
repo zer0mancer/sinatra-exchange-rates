ababd51350c1238dded51484b3dfe12748852dca
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-03-22 17:55:00 +0000

    added conversion

--- app.rb
+++ app.rb
@@ -23,3 +23,31 @@ get("/") do
   # render a view template where I show the symbols
    erb(:homepage)
 end
+
+get("/:first_currency") do
+
+  @original_currency = params.fetch("first_currency")
+
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @symbols = parsed_data.fetch("currencies").keys
+
+  erb(:first_currency)
+end
+
+
+  get("/first_currency/:second_currency") do
+    @original_currency = params.fetch("first_currency")
+    @converted_currency = params.fetch("second_currency")
+
+    api_url = "http://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
+    raw_data = HTTP.get(api_url)
+    raw_data_string = raw_data.to_s
+    parsed_data = JSON.parse(raw_data_string)
+    @symbols = parsed_data.fetch("currencies").keys
+
+
+    erb(:conversion)
+  end
